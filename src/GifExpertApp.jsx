2b04829aaabf57e25b0863d0b9d782eb3e364f6c
Yinner Chilito <yinnermedina30@gmail.com>
2023-09-27 09:01:49 -0500

    feature: Creando una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setcategories] = useState(["Goku", "Vegeta"]);
+  console.log(categories);
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+        {/* <li>123</li>
+        <li>XYZ</li> */}
+      </ol>
     </>
   );
 };
