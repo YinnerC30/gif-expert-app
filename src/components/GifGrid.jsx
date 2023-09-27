d9752726620a5cbb9eafdbc2269421c0b5dc6ba4
Yinner Chilito <yinnermedina30@gmail.com>
2023-09-27 10:39:07 -0500

    feature: useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,7 +1,10 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
   return (
     <>
